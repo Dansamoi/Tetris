68daae9976acfc125f61cf7dbfd84fd1a5a240b7
Dansamoi <dankot2004@gmail.com>
2021-10-18 22:41:21 +0300

    Base game engine

---
+++ Tetris1/Game.hpp
@@ -0,0 +1,25 @@
+#include "SDL.h"
+#include <iostream>
+
+class Game
+{
+private:
+	int cnt = 0;
+	bool isRunning;
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+public:
+	Game();
+	~Game();
+
+	void init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+	
+	void handleEvents();
+	void update();
+	void render();
+	void clean();
+
+	bool running() { return isRunning; };
+
+};
+
